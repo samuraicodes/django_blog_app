113551a2b61589be6c81f3637f870f0ff9f9fb27
Samurai Codes <samurais.codes@gmail.com>
2019-08-15 21:55:28 +0200

    Fix: Change link. Bad Request (400)

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&ld%%-ek$qjnavdq&1dx-2w@#z(-@&=cyvc#3u5_&te9!@@ktn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'http://samuraicodes.pythonanywhere.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'samuraicodes.pythonanywhere.com']
 
 
 # Application definition
