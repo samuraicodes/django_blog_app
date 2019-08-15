95275bb2777bf45fa882ab77ea950f26516decec
Samurai Codes <samurais.codes@gmail.com>
2019-08-15 21:37:40 +0200

    Chore: Debug to False

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&ld%%-ek$qjnavdq&1dx-2w@#z(-@&=cyvc#3u5_&te9!@@ktn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'http://samuraicodes.pythonanywhere.com/']
 
