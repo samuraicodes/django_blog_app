cc318d3b6dd712530cc8814e875dad2d78c8d341
Samurai Codes <samurais.codes@gmail.com>
2019-08-15 23:02:44 +0200

    Feat: Html added.

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&ld%%-ek$qjnavdq&1dx-2w@#z(-@&=cyvc#3u5_&te9!@@ktn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'samuraicodes.pythonanywhere.com']
 
